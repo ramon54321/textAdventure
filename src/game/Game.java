c937a01fb099d5fa9f0f874825b40bf77b289008
ramon54321 <ramon2009@gmail.com>
2015-12-23 14:41:16 +0200

    Added render and tick and set up buffer strategy.

--- src/game/Game.java
+++ src/game/Game.java
@@ -2,6 +2,8 @@ package game;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
 /**
  * Created by ramon on 20.12.2015.
@@ -10,13 +12,24 @@ import java.awt.*;
 
 public class Game extends Canvas implements Runnable{
 
-    public static Thread gameThread;
+    //TODO: Add static reference to game thread.
 
+    // Global variables
     private final int width = 300;
     private final int height = width / 16 * 9;
     private final int scale = 1;
+    private final int targetFPS = 144;
+    private final int targetTPS = 60;
 
+    // Reference Variables
     private JFrame mainFrame;
+
+    // Management variables
+    private int framesRendered = 0;
+    private int framesTicked = 0;
+    private int lastFPS;
+    private int lastTPS;
+    private BufferedImage mainRenderImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
     public Game() {
         System.out.println("Started...");
@@ -24,8 +37,60 @@ public class Game extends Canvas implements Runnable{
     }
 
     public void run(){
-        //TODO: Calculate Tick and Update loops, and call corresponding functions.
+        // This method is run in a new thread and loops constantly in while.
+        long lastTimeTick = System.nanoTime();
+        long lastTimeRender = System.nanoTime();
+        long lastTimeClock = System.nanoTime();
+        while(true){
+            long currentTime = System.nanoTime();
+
+            // Render
+            if(currentTime - lastTimeRender > ((1 * 1000000000) / targetFPS)){
+                render();
+                framesRendered++;
+                lastTimeRender = currentTime;
+            }
+
+            // Tick
+            if(currentTime - lastTimeTick > ((1 * 1000000000) / targetTPS)){
+                tick();
+                framesTicked++;
+                lastTimeTick = currentTime;
+            }
+
+            // Clock
+            if(currentTime - lastTimeClock > (1 * 1000000000)){
+                lastFPS = framesRendered;
+                lastTPS = framesTicked;
+                framesRendered = 0;
+                framesTicked = 0;
+                System.out.println("FPS: " + lastFPS);
+                System.out.println("TPS: " + lastTPS);
+                lastTimeClock = currentTime;
+            }
+        }
     }
+
+    private void render(){
+        BufferStrategy bs = getBufferStrategy();
+        if(bs == null){
+            createBufferStrategy(3);
+            return;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+        g.drawImage(mainRenderImage, 0, 0, getWidth(), getHeight(), null);
+        g.dispose();
+
+        bs.show();
+    }
+
+    private void tick(){
+
+    }
+
+    //TODO: Add Render and Tick behaviour
+    //TODO: Add tiles and render tiles
 
     private void initGame(){
         mainFrame = new JFrame();
